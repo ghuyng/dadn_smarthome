7c37cbfecf961943aa8f0ed193c468e94ab6c2ad
Hung <hung.dinh.phuc@hcmut.edu.vn>
2021-05-20 12:56:45 +0700

    sign up interface partially completed

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -1,35 +1,36 @@
 package com.example.smarthome
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.content.Intent
+import android.widget.TextView
+import android.widget.EditText
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.smarthome.databinding.ActivityMainBinding
+import com.example.smarthome.databinding.ActivityAuthenticateBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityAuthenticateBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val contxt = this
+        binding = ActivityAuthenticateBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val button: Button = findViewById(R.id.button_sign_in)
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d("SignIn", "onClick: called")
+                val intent = Intent(contxt, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
