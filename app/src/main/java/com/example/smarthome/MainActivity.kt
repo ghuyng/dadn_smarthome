5f912cb9a62e9c0e524d4331b2d3bb235b8077cf
Hung <hung.dinh.phuc@hcmut.edu.vn>
2021-05-21 11:08:12 +0700

    Sign in/up basically finished, Sign in is in MainActivity.kt

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -24,10 +24,25 @@ class MainActivity : AppCompatActivity() {
         val contxt = this
         binding = ActivityAuthenticateBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val button: Button = findViewById(R.id.button_sign_in)
-        button.setOnClickListener(object : View.OnClickListener {
+        val user_email: EditText = findViewById(R.id.editTextTextEmailAddress)
+        val user_password: EditText = findViewById(R.id.editTextTextPassword)
+        val buttonSignIn: Button = findViewById(R.id.button_sign_in)
+        val textViewSignUp: TextView = findViewById(R.id.signUpTextView)
+        buttonSignIn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 Log.d("SignIn", "onClick: called")
+                val intent = Intent(contxt, SignUpActivity::class.java)
+                Log.d("SignIn", "Email: ${user_email.text.toString()}")
+                Log.d("SignIn", "Password: ${user_password.text.toString()}")
+
+                intent.putExtra("UserEmail",user_email.toString())
+                intent.putExtra("UserPassword",user_password.toString())
+                // startActivity(intent)
+            }
+        })
+        textViewSignUp.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Log.d("SignUp", "onClick: called")
                 val intent = Intent(contxt, SignUpActivity::class.java)
                 startActivity(intent)
             }
--- app/src/main/java/com/example/smarthome/SignUpActivity.kt
+++ app/src/main/java/com/example/smarthome/SignUpActivity.kt
@@ -1,12 +1,13 @@
 package com.example.smarthome
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.smarthome.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -19,6 +20,31 @@ class SignUpActivity : AppCompatActivity() {
 
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val contxt = this
+        val buttonSignUp: Button = findViewById(R.id.btnSignUp)
+        val buttonBack: Button = findViewById(R.id.btnBackToSignIn)
+        val user_name: EditText = findViewById(R.id.textNameSignUp)
+        val user_email: EditText = findViewById(R.id.textEmailSignUp)
+        val user_password: EditText = findViewById(R.id.textPasswordSignUp)
+        val user_passwordRe: EditText = findViewById(R.id.textPasswordRetypeSignUp)
+        buttonSignUp.setOnClickListener {
+            Log.d("SignUp", "onClick: called")
+            val intent = Intent(contxt, MainActivity::class.java)
+            Log.d("SignUp", "Name: ${user_name.text.toString()}")
+            Log.d("SignUp", "Email: ${user_email.text.toString()}")
+            Log.d("SignUp", "Password: ${user_password.text.toString()}")
+            Log.d("SignUp", "Password Retype: ${user_passwordRe.text.toString()}")
 
+            intent.putExtra("NewUserName", user_name.toString())
+            intent.putExtra("NewUserEmail", user_email.toString())
+            intent.putExtra("NewUserPassword", user_password.toString())
+            intent.putExtra("NewUserPasswordRetype", user_passwordRe.toString())
+            // startActivity(intent)
+        }
+        buttonBack.setOnClickListener {
+            Log.d("SignUpBack", "onClick: called")
+            val intent = Intent(contxt, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
